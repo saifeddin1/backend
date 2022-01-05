33c7b1de3f9a7b0ee69bb6d535987b56b1d92ed0
saifeddin1 <saifeddinmatoui4@gmail.com>
2022-01-05 10:07:51 +0100

    final i swear

--- courses/serializers.py
+++ courses/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import routers, serializers, viewsets
 from .models import Course
 
 # Serializers define the API representation.
+
+
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    course_classe = serializers.ReadOnlyField(source='classe.name')
+
     class Meta:
         model = Course
-        fields = ['id', 'title', 'content']
-
-        
-
+        fields = ['id', 'title', 'content', 'classe', 'course_classe']
